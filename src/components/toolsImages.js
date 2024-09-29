d12870f5e6cb8cfe10bed83151fa0002e2a65fb5
AndreaJQ <113391766+AndreaJQ@users.noreply.github.com>
2024-09-29 02:57:36 +0000

    tools

---
+++ src/components/toolsImages.js
@@ -0,0 +1,22 @@
+import fcp from "../assets/tools/final_cut_pro.png";
+import gsuite from "../assets/tools/gsuite.png";
+import intellij from "../assets/tools/intellij.png";
+import office from "../assets/tools/office.png"
+import emule from "../assets/tools/emule.png"
+import audacity from "../assets/tools/Audacity.png"
+import icloud from "../assets/tools/icloud.ico"
+import trello from "../assets/tools/Trello-icon.png"
+
+
+export const toolsImages =[
+    { id: 1, imgUrl: "../assets/tools/final_cut_pro.png"},
+    { id: 2, imgUrl: "../assets/tools/gsuite.png"},
+    { id: 3, imgUrl: "../assets/tools/intellij.png"},
+    { id: 4, imgUrl: "../assets/tools/office.png"},
+    { id: 5, imgUrl: "../assets/tools/emule.png"},
+    { id: 6, imgUrl: "../assets/tools/Audacity.png"},
+    { id: 7, imgUrl: "../assets/tools/icloud.ico"},
+    { id: 8, imgUrl: "../assets/tools/Trello-icon.png"}
+    
+
+]
